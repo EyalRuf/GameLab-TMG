6c04bdd57f23528675bf4b8e6f4dc2b3c5c2e563
Eyal Rufeisen <eyalrocks@gmail.com>
2021-03-03 14:31:09 +0100

    Added 3rd person controller

---
+++ Assets/Scripts/LocalPlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalPlayerInput : MonoBehaviour
+{
+    public Vector3 moveInput = Vector3.zero;
+    public bool jumpInput = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ver = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(ver) < 0.01f)
+            ver = 0;
+        if (Mathf.Abs(hor) < 0.01f)
+            hor = 0;
+
+        moveInput = new Vector3(hor, 0, ver);
+        jumpInput = Input.GetKey(KeyCode.Space);
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    private LocalPlayerInput lpInput;
+    private Rigidbody rb;
+
+    [Header("Movement")]
+    public float speed;
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundCheckMask;
+    public float jumpForce;
+
+    void Awake()
+    {
+        lpInput = GetComponent<LocalPlayerInput>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundCheckMask).Length > 0;
+        if (isGrounded && lpInput.jumpInput)
+        {
+            rb.AddForce(jumpForce * Vector3.up, ForceMode.Acceleration);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.transform.Translate((lpInput.moveInput * speed * Time.deltaTime), Space.Self);
+    }
+}
